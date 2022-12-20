9ff08b826f358fd42b53ffe82196148ea4b63c46
gldeng <realgldeng@gmail.com>
2022-12-20 16:28:16 +0800

    Clean up build script #14

--- build.rs
+++ build.rs
@@ -1,17 +1,4 @@
-use std::{env, fs};
-use std::fs::File;
-use std::io::Read;
-use std::path::Path;
-
-// fn copy_files(from:&str, to: &str) -> Result<(), Box<dyn std::error::Error>> {
-//     let out_dir = env::var("OUT_DIR")?;
-//     let src_path = Path::new(&out_dir).join(from);
-//     let dest_path = Path::new(to);
-//     let mut src_file = File::open(src_path)?;
-//     let mut contents = String::new();
-//     src_file.read_to_string(&mut contents)?;
-//     fs::write(&dest_path, contents)?;
-// }
+use std::env;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     if let Ok(yes) = env::var("GENERATE_PROTOBUF") {
@@ -28,14 +15,3 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
     Ok(())
 }
-// use prost_build;
-// use std::io::Result;
-// use env;
-// use fs;
-// fn main() -> Result<()> {
-//     prost_build::compile_protos(&["proto/mpc.proto"], &["proto"])?;
-//     // let out_dir = env::var_os("OUT_DIR").unwrap();
-//     // let dest_path = Path::new(&out_dir).join("mpc.rs");
-//     // fs::write(&dest_path, code).unwrap();
-//     Ok(())
-// }
